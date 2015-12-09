36c04d2714bcda5541cfa193093fa3db015422d6
Joost Lubach <joost@yoazt.com>
2015-12-09 21:45:54 +0100

    Renamed MDKit -> Doctor

---
+++ Doctor.h
@@ -0,0 +1,19 @@
+//
+//  Doctor.h
+//  Doctor
+//
+//  Created by Joost Lubach on 04/12/15.
+//  Copyright © 2015 Mos Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Doctor.
+FOUNDATION_EXPORT double DoctorVersionNumber;
+
+//! Project version string for Doctor.
+FOUNDATION_EXPORT const unsigned char DoctorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Doctor/PublicHeader.h>
+
+
