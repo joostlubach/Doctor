1d80586b26026b72d8465f34e2b3357e90e3411d
Joost Lubach <joost@yoazt.com>
2015-12-11 14:17:53 +0100

    Added bounce() quick method

--- Code/UIView+Bounce.swift
+++ Code/UIView+Bounce.swift
@@ -1,9 +1,23 @@
-//
-//  UIView+Bounce.swift
-//  Doctor
-//
-//  Created by Joost Lubach on 11/12/15.
-//  Copyright © 2015 Mos Dev. All rights reserved.
-//
+import UIKit
 
-import Foundation
+public extension UIView {
+
+  func bounce(scale scale: CGFloat = 1.2, duration: NSTimeInterval = 0.3) {
+    layer.bounce(scale: scale, duration: duration)
+  }
+
+
+}
+
+public extension CALayer {
+
+  func bounce(scale scale: CGFloat, duration: CFTimeInterval) {
+    let anim = CAKeyframeAnimation(keyPath: "transform.scale")
+    anim.duration = duration
+    anim.keyTimes = [0.0, 0.3, 1.0]
+    anim.values = [1.0, scale, 1.0]
+    anim.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseIn)]
+    addAnimation(anim, forKey: "pulsate")
+  }
+
+}
