0958d4e35b9b11e51114919abe113eac59e6bcfd
Joost Lubach <joost@yoazt.com>
2016-01-14 17:04:17 +0100

    Added hex string to UIColor

--- Code/UIColor+Hex.swift
+++ Code/UIColor+Hex.swift
@@ -9,5 +9,26 @@ public extension UIColor {
 
     self.init(red: red, green: green, blue: blue, alpha: 1.0)
   }
+
+  var hex: Int {
+    var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0
+    var alpha: CGFloat = 0
+    getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+
+    return Int(red*255) << 16 | Int(green*255) << 8 | Int(blue*255)
+  }
+
+  var hexString: String {
+    var red: CGFloat = 0, green: CGFloat = 0, blue: CGFloat = 0
+    var alpha: CGFloat = 0
+    getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+
+    let string = NSMutableString()
+    string.appendFormat("%02hhX", Int(red * 255))
+    string.appendFormat("%02hhX", Int(green * 255))
+    string.appendFormat("%02hhX", Int(blue * 255))
+
+    return string as String
+  }
   
 }
